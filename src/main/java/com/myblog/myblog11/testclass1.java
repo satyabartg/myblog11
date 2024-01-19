b115298b74288cc6e0da8a5d06a6609960b0c3f9
satyabartg <ak4304015@gmail.com>
2024-01-19 15:19:16 +0530

    127:Create save Post Feature Entity class added 10th jan 2024

--- src/main/java/com/myblog/myblog11/testclass1.java
+++ src/main/java/com/myblog/myblog11/testclass1.java
@@ -3,14 +3,35 @@ package com.myblog.myblog11;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Predicate;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class testclass1 {
     public static void main(String[] args) {
-      Consumer<Integer> result=number-> System.out.println(number);
-      result.accept(100);
+        List<Employee> employees = Arrays.asList(new Employee("mike",20,"odisha"),
+                                          new Employee("stalin",21,"channai"),
+                                          new Employee("Manju",31,"kalkota"),
+                                          new Employee("pinku",32,"banglore")
+
+        );
+        Map<Integer, List<Employee>> collect = employees.stream().collect(Collectors.groupingBy(emp -> emp.getAge()));
+//        List<Employee> emps = employees.stream().filter(emp -> emp.getAge() >30).collect(Collectors.toList());
+//        List<Employee> emp1 = employees.stream().filter(emp -> emp.getName().toLowerCase().startsWith("m")).collect(Collectors.toList());
+        System.out.println(collect);
+        {
+            for (Employee employee:employees) {
+                List<Employee> emp = collect.get(employees.get(0).getAge());
+
+                for (Employee x : emp) {
+                    System.out.println(x.getName());
+                    System.out.println(x.getCity());
+                    System.out.println(x.getAge());
+                }
+            }
+        }
+
     }
+
 }
+
+
